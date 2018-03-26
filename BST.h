125971381d53099a731b4005a8ac7159719695c3
Manuel Berrueta <manuelberrueta@gmail.com>
2018-03-25 23:55:35 -0700

    Updated first commit H

--- BST.h
+++ BST.h
@@ -5,12 +5,21 @@
 class BST 
 {
 public:
+	//Constructor must open, read "MorseTable.txt", creat nodes for ea char insert nodes into tree
+	//using insert function, close the file
 	BST();
 
 	bool isEmpty();
 
+	//print() recursively prints BST in order (left most pritnted first)
+	//search() will return morse code for each English char
+
+	//destructor that destroys the tree
+	//~BST();
+
 
 private:
 	BSTNode * pRoot;
+	//void insert(BSTNode *& pTree, const string & info);
 
 };
